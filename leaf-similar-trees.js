d9b332a3ba299eb0d986f832226a6635ff7a30f5
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-01-09 23:27:38 +0530

    Update leaf-similar-trees.js

--- leaf-similar-trees.js
+++ leaf-similar-trees.js
@@ -1,3 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
 var leafSimilar = function(root1, root2) {
     const rec = function(node, arr) {
         if (node === null) {
@@ -9,3 +22,9 @@ var leafSimilar = function(root1, root2) {
         rec(node.left, arr);
         rec(node.right, arr);
     }
+    const a1 = [];
+    const a2 = []
+    rec(root1, a1);
+    rec(root2, a2);
+    return a1.toString() === a2.toString();
+};
