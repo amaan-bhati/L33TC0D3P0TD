859b66aa96db8d1aa8183207493476f260588b57
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-01-24 23:43:30 +0530

    Update convert-sorted-list-to-binary-search-tree.js

--- convert-sorted-list-to-binary-search-tree.js
+++ convert-sorted-list-to-binary-search-tree.js
@@ -1,1 +1,21 @@
-
+var sortedListToBST = function (head) {
+    const listtoarr = (head) => {
+        let stack = [];
+        while (head) {
+            stack.push(head.val);
+            head = head.next;
+        }
+        return stack;
+    }
+    let arrayfromlist = listtoarr(head);
+    const dfs = (left, right) => {
+        if (left > right) return null;
+        let mid = ~~((left + right) / 2);
+        let newnode = new TreeNode(arrayfromlist[mid]);
+        newnode.left = dfs(left, mid - 1);
+        newnode.right = dfs(mid + 1, right);
+        return newnode;
+    }
+    return dfs(0, arrayfromlist.length - 1);
+};
+                                                                        
