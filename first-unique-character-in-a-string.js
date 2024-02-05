56f3abec9e4dcf2fffb918a2d059112aca089a28
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-02-05 23:38:23 +0530

    Update first-unique-character-in-a-string.js

--- first-unique-character-in-a-string.js
+++ first-unique-character-in-a-string.js
@@ -1,1 +1,14 @@
+var firstUniqChar = function (s) {
 
+    for (let i = 0; i < s.length; i++) {
+
+        let firstIndex = s.indexOf(s[i])
+        let lastIndex = s.lastIndexOf(s[i])
+
+        if (firstIndex === lastIndex) {
+            return i
+        }
+    }
+
+    return -1
+};
