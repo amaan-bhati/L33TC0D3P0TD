6431a53a9d488c77ba4394c421c5c4677f6b08a2
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-02-21 21:23:13 +0530

    Create bitwise-and-of-numbers-range.js

---
+++ bitwise-and-of-numbers-range.js
@@ -0,0 +1,10 @@
+var rangeBitwiseAnd = function (left, right) {
+
+    let count = 0;
+    while (left != right) {
+        left >>= 1;
+        right >>= 1;
+        count++;
+    }
+    return left << count;
+};
