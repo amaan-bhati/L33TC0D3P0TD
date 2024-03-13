305e4bcca44ef4a2ad6db23676e861b7f3b95609
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-03-13 22:56:43 +0530

    Update find-the-pivot-integer.js

--- find-the-pivot-integer.js
+++ find-the-pivot-integer.js
@@ -1,1 +1,17 @@
-
+var pivotInteger = function (n) {
+    if (n === 1) return 1;
+    let sumLeft = 0;
+    let sumRight = 0;
+    let i = 0;
+    while (i < n) {
+        if (sumLeft <= sumRight) {
+            sumLeft += i;
+            i++
+        } else if (sumLeft > sumRight) {
+            sumRight += n;
+            n--;
+        }
+    }
+    if (sumLeft === sumRight) return n;
+    return -1;
+};
