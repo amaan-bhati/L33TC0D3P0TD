b995b52e1659e3a8c32504ebcf22ab512bae3bf9
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-03-14 23:54:08 +0530

    Update binary-subarrays-with-sum.js

--- binary-subarrays-with-sum.js
+++ binary-subarrays-with-sum.js
@@ -1,1 +1,18 @@
+var numSubarraysWithSum = function (nums, goal) {
+    let totalSubarray = 0;
 
+    for (let i = 0; i < nums.length; i++) {
+        let sum = 0;
+        for (let j = i; j < nums.length; j++) {
+            sum += nums[j]
+            if (sum === goal) {
+                totalSubarray++
+            }
+            if (sum > goal) {
+                break
+            }
+        }
+    }
+
+    return totalSubarray
+};
