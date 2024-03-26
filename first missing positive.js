6f7ec97fcde1a741b9101f85d410efe2ebc53bb4
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-03-26 10:06:47 +0530

    Create first missing positive.js

---
+++ first missing positive.js
@@ -0,0 +1,17 @@
+var firstMissingPositive = function (nums) {
+    var n = nums.length;
+    for (var i = 0; i < n; i++) {
+        while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+            var temp = nums[nums[i] - 1];
+            nums[nums[i] - 1] = nums[i];
+            nums[i] = temp;
+        }
+    }
+    for (var i = 0; i < n; i++) {
+        if (nums[i] != i + 1) {
+            return i + 1;
+        }
+    }
+
+    return n + 1;
+};
