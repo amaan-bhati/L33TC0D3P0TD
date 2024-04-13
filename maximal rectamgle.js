049872f0de8c8ec750744513c2dcfd3bef4c1d8b
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-04-13 21:09:27 +0530

    Update maximal rectamgle.js

--- maximal rectamgle.js
+++ maximal rectamgle.js
@@ -1,1 +1,41 @@
 
+function maximalRectangle(matrix) {
+    if (matrix.length === 0) return 0;
+
+    const numRows = matrix.length;
+    const numCols = matrix[0].length;
+    const heights = Array(numCols).fill(0);
+    let maxArea = 0;
+
+    for (let i = 0; i < numRows; i++) {
+        for (let j = 0; j < numCols; j++) {
+            if (matrix[i][j] === "1") {
+                heights[j]++;
+            } else {
+                heights[j] = 0;
+            }
+        }
+
+        maxArea = Math.max(maxArea, largestRectangleArea(heights));
+    }
+
+    return maxArea;
+}
+
+function largestRectangleArea(heights) {
+    const stack = [];
+    let maxArea = 0;
+
+    for (let i = 0; i <= heights.length; i++) {
+        while (stack.length > 0 && (i === heights.length || heights[i] < heights[stack[stack.length - 1]])) {
+            const height = heights[stack.pop()];
+            const width = stack.length === 0 ? i : i - stack[stack.length - 1] - 1;
+            maxArea = Math.max(maxArea, height * width);
+        }
+
+        stack.push(i);
+    }
+
+    return maxArea;
+}
+                                                                                                                                                                                    
