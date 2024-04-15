ea0f5a73a18da25c98feef5976aa855fd31b0592
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-04-15 23:07:34 +0530

    Update sum-root-to-leaf-numbers.js

--- sum-root-to-leaf-numbers.js
+++ sum-root-to-leaf-numbers.js
@@ -1,1 +1,14 @@
-
+var sumNumbers = function (root) {
+    const traverse = (root, sum) => {
+        sum = sum ? Number("" + sum + root.val) : root.val
+        if (root.left === null && root.right === null) {
+            return sum
+        } else if (root.left === null) {
+            return traverse(root.right, sum)
+        } else if (root.right === null) {
+            return traverse(root.left, sum)
+        }
+        return traverse(root.left, sum) + traverse(root.right, sum)
+    }
+    return traverse(root, null)
+};
