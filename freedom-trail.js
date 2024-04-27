0065268a7d02814e063c7d570606e89454cf2d6a
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-04-27 22:52:50 +0530

    Update freedom-trail.js

--- freedom-trail.js
+++ freedom-trail.js
@@ -1,1 +1,17 @@
+var findRotateSteps = function (ring, key) {
+    let currentPosition = 0;
 
+    let steps = 0;
+
+    for (let i = 0; i < key.length; i++) {
+         let difference = (key[i].charCodeAt(0) - ring[currentPosition].charCodeAt(0) + 26) % 26;
+
+         steps += Math.min(difference, 26 - difference);
+
+        steps++;
+
+        currentPosition += difference;
+    }
+
+     return steps;
+};
