30251a851e311050964bc2146342224f18c32b38
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-05-02 20:51:48 +0530

    Update largest-positive-integer-that-exists-with-its-negative.js

--- largest-positive-integer-that-exists-with-its-negative.js
+++ largest-positive-integer-that-exists-with-its-negative.js
@@ -1,1 +1,13 @@
-
+var findMaxK = function (nums) {
+    let maxNeg = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] < maxNeg && nums.includes(Math.abs(nums[i]))) {
+            maxNeg = nums[i]
+        }
+    }
+    if (maxNeg < 0) {
+        return Math.abs(maxNeg)
+    } else {
+        return -1;
+    }
+};
