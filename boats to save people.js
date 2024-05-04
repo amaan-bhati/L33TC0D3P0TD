2198a8a50f3f164a5e335fc418a370e3cf887ebe
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-05-04 22:01:19 +0530

    Update boats to save people.js

--- boats to save people.js
+++ boats to save people.js
@@ -1,1 +1,21 @@
+var numRescueBoats = function(people, limit) {
+    people.sort((a,b) => a- b);
+    const n = people.length;
+    let i = 0;
+    let j = n - 1;
+    let boatReq = 0;
 
+    while(i <= j) {
+      if((people[i] + people[j]) <= limit) {
+        i++;
+        j--;
+      }else {
+        if(people[j] <= limit) 
+          j--;
+        else 
+          i++;
+      }
+      boatReq++;
+    }
+    return boatReq;
+};
