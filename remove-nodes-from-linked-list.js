1490fbc9fe6b6b695e00c013c26f5d03224e31c4
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-05-06 23:38:42 +0530

    Update remove-nodes-from-linked-list.js

--- remove-nodes-from-linked-list.js
+++ remove-nodes-from-linked-list.js
@@ -1,1 +1,25 @@
 
+var reverseList = (node) => {
+    let prev = null;
+    while (node) {
+        let future = node.next;
+        node.next = prev;
+        prev = node;
+        node = future;
+    }
+    return prev;
+}
+var removeNodes = function (head) {
+    let temp = reverseList(head);
+    let curr = temp;
+    let prev = curr;
+    while (curr) {
+        if (prev.val > curr.val) {
+            prev.next = curr.next;
+        } else {
+            prev = curr;
+        }
+        curr = curr.next;
+    }
+    return reverseList(temp);
+};
