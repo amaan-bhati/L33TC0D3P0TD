1604606f4148c4a916872add6b65008cfb16edf2
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-05-08 22:45:11 +0530

    Update relative-rank.js

--- relative-rank.js
+++ relative-rank.js
@@ -1,1 +1,17 @@
 
+const PRIZE_TITLES = ['Gold Medal', 'Silver Medal', 'Bronze Medal'];
+var findRelativeRanks = function (scores) {
+    const prizes = [];
+
+    scores
+        .reduce((acc, score, i) => {
+            acc.push([score, i]);
+            return acc;
+        }, [])
+        .sort(([a], [b]) => b - a)
+        .forEach(([, index], i) => {
+            prizes[index] = PRIZE_TITLES[i] ?? String(i + 1);
+        });
+
+    return prizes;
+};
