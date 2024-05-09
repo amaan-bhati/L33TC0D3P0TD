1ef1e592da5532894643d78d1c3f91bad8acdc3d
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-05-09 22:53:37 +0530

    Update maximise happiness of selected students.js

--- maximise happiness of selected students.js
+++ maximise happiness of selected students.js
@@ -1,1 +1,11 @@
 
+var maximumHappinessSum = function (happiness = [], k = 0) {
+    happiness.sort((a, b) => b - a);
+    let max = 0;
+
+    for (let i = 0; i < k; i++) {
+        max += happiness[i] - i <= 0 ? 0 : happiness[i] - i;
+    }
+
+    return max;
+};
