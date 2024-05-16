fcc057dc9e644c4df67be223595221792f32375c
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-05-16 22:39:14 +0530

    Update evaluate-boolean-binary-tree.js

--- evaluate-boolean-binary-tree.js
+++ evaluate-boolean-binary-tree.js
@@ -1,1 +1,13 @@
-
+var evaluateTree = function (root) {
+    function Recurse(node) {
+        if (node.val === 0 || node.val === 1) {
+            return node.val === 1;
+        } else if (node.val === 2) {
+            return Recurse(node.left) || Recurse(node.right);
+        } else if (node.val === 3) {
+            return Recurse(node.left) && Recurse(node.right);
+        }
+        return false;
+    }
+    return Recurse(root);
+};
