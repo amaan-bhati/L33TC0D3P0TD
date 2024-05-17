f34424c2989501f5ea26dce9d4a33c51c0ad99b5
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-05-17 18:47:39 +0530

    Update delete-leaves-with-a-given-value.js

--- delete-leaves-with-a-given-value.js
+++ delete-leaves-with-a-given-value.js
@@ -1,1 +1,9 @@
-
+var removeLeafNodes = function (root, target) {
+    if (root === null) return null;
+    root.left = removeLeafNodes(root.left, target);
+    root.right = removeLeafNodes(root.right, target);
+    if (root.val === target && root.left === null && root.right === null) {
+        return null;
+    }
+    return root;
+};
