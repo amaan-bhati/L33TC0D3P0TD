d7f985c066a5b78a843337020a3f325cd460468f
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-05-18 23:03:30 +0530

    Update distribute-coins-in-binary-tree.js

--- distribute-coins-in-binary-tree.js
+++ distribute-coins-in-binary-tree.js
@@ -1,1 +1,12 @@
-
+var distributeCoins = function (root) {
+    let moves = 0;
+    var dfs = (root) => {
+        if (root === null) return 0;
+        let left = dfs(root.left)
+        let right = dfs(root.right);
+        moves += Math.abs(left) + Math.abs(right);
+        return root.val + left + right - 1
+    }
+    dfs(root);
+    return moves
+};
