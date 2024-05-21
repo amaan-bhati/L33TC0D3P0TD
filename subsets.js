cf762aea8cdb0e01c982d5f167e200c11f53849f
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-05-21 20:50:36 +0530

    Update subsets.js

--- subsets.js
+++ subsets.js
@@ -1,1 +1,15 @@
+var subsets = function (nums) {
+    const result = [];
 
+    function backtrack(start, path) {
+        result.push([...path]);
+        for (let i = start; i < nums.length; i++) {
+            path.push(nums[i]);
+            backtrack(i + 1, path);
+            path.pop();
+        }
+    }
+
+    backtrack(0, []);
+    return result;
+};
