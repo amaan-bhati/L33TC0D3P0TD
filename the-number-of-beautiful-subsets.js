c1931e546ac262602a9313fcf6c54bf9f93b4434
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-05-23 22:18:52 +0530

    Update the-number-of-beautiful-subsets.js

--- the-number-of-beautiful-subsets.js
+++ the-number-of-beautiful-subsets.js
@@ -1,1 +1,20 @@
+var beautifulSubsets = function (nums, k) {
+    var bt = (idx, arr) => {
+        if (idx === nums.length) {
+            if (arr.length) return 1;
+            return 0;
+        }
 
+        let res = 0;
+        if (!arr.includes(nums[idx] - k) && !arr.includes(nums[idx] + k)) {
+            arr.push(nums[idx]);
+            res += bt(idx + 1, arr);
+            arr.pop();
+        }
+        res += bt(idx + 1, arr);
+
+        return res;
+    }
+
+    return bt(0, []);
+};
