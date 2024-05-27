57e43f020052a7e5dc88acd69ad53cb2630dc53f
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-05-27 22:43:30 +0530

    Update special-array-with-x-elements-greater-than-or-equal-x.js

--- special-array-with-x-elements-greater-than-or-equal-x.js
+++ special-array-with-x-elements-greater-than-or-equal-x.js
@@ -1,1 +1,9 @@
+var specialArray = function (nums) {
+    nums.sort(function (a, b) { return a - b });
+    const l = nums.length;
 
+    for (let spec = nums.length; spec > 0; spec--) {
+        if (spec <= nums[l - spec] && ((l - spec == 0) || (spec > nums[l - spec - 1]))) return spec;
+    }
+    return -1;
+};
