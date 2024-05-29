14a07c3661d6b16849cb6ae71aa1cbac0a81da6f
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-05-29 20:55:52 +0530

    Update reduce-a-number-in-binary-representation-to-one.js

--- reduce-a-number-in-binary-representation-to-one.js
+++ reduce-a-number-in-binary-representation-to-one.js
@@ -1,1 +1,21 @@
 
+/**
+ * @param {string} s
+  * @return {number}
+   */
+var numSteps = function (s) {
+    let num = BigInt("0b" + s);
+    let count = 0;
+    if (num === 1n) {
+        return count
+    }
+    while (num > 1n) {
+        count = count + 1
+        if (num % 2n !== 0n) {
+            num = num + 1n
+        } else {
+            num = num / 2n
+        }
+    }
+    return count
+};
