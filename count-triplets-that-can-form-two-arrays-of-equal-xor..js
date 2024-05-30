9d3ea42ec63f28b201913167f198fdd1b615713d
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-05-30 22:54:57 +0530

    Update count-triplets-that-can-form-two-arrays-of-equal-xor..js

--- count-triplets-that-can-form-two-arrays-of-equal-xor..js
+++ count-triplets-that-can-form-two-arrays-of-equal-xor..js
@@ -1,1 +1,20 @@
 
+var countTriplets = function (arr) {
+    let n = arr.length;
+    let count = 0;
+    let prefixXOR = new Array(n + 1).fill(0);
+
+    for (let i = 0; i < n; ++i) {
+        prefixXOR[i + 1] = prefixXOR[i] ^ arr[i];
+    }
+
+    for (let i = 0; i < n; ++i) {
+        for (let j = i + 1; j < n; ++j) {
+            if (prefixXOR[i] == prefixXOR[j + 1]) {
+                count += (j - i);
+            }
+        }
+    }
+
+    return count;
+};
