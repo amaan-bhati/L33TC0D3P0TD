cdd84de4e12120d0a2eceff685428ff122a84301
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-05-31 23:49:44 +0530

    Update single-number-3.js

--- single-number-3.js
+++ single-number-3.js
@@ -1,1 +1,14 @@
-
+var singleNumber = function(nums) {
+    const numbers={};
+    const singleNum=[]
+    for(let i=0; i<nums.length; i++){
+        if(!numbers.hasOwnProperty(nums[i])){
+            numbers[nums[i]]=i;
+            singleNum.push(nums[i]);
+        } else {
+            let index = singleNum.indexOf(nums[i]);
+            singleNum.splice(index, 1);
+        }
+    }
+    return singleNum;
+};
