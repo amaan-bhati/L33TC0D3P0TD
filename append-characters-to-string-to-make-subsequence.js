8ebb304ca410a0f22c7c6337d5f972b6bb875c93
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-06-03 23:55:29 +0530

    Update append-characters-to-string-to-make-subsequence.js

--- append-characters-to-string-to-make-subsequence.js
+++ append-characters-to-string-to-make-subsequence.js
@@ -1,1 +1,13 @@
+var appendCharacters = function (s, t) {
+    let ln = t.length;
+    let j = 0;
 
+    for (let i = 0; i < s.length; i++) {
+
+        if (s[i] === t[j]) {
+            j++;
+            ln--;
+        }
+    }
+    return ln;
+};
