de277b24d10762806df675f9dd6d380093d73bd1
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-06-05 22:09:47 +0530

    Update find-common-charecters.js

--- find-common-charecters.js
+++ find-common-charecters.js
@@ -1,1 +1,15 @@
 
+var commonChars = function (words) {
+    let result = [];
+
+    for (let char of words[0]) {
+
+        if (words.every(word => word.includes(char))) {
+            result.push(char);
+
+            words = words.map(word => word.replace(char, ""));
+        }
+    }
+
+    return result;
+};
