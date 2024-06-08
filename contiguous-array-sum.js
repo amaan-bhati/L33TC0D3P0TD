aef786be4783e3d21d7a2040c339b624d20ef86a
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-06-08 23:11:04 +0530

    Update contiguous-array-sum.js

--- contiguous-array-sum.js
+++ contiguous-array-sum.js
@@ -1,1 +1,15 @@
-
+var checkSubarraySum = function (nums, k) {
+    let hash = new Map();
+    let i = 0;
+    let sum = 0;
+    hash.set(0, 0)
+    while (i < nums.length) {
+        sum += nums[i] % k;
+        if (!hash.has(sum % k)) hash.set(sum % k, i + 1);
+        else {
+            if (i - hash.get(sum % k) + 1 >= 2) return true;
+        }
+        i++;
+    }
+    return false;
+};
