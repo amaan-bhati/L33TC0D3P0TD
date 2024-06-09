ea93ff30e924f36042af9f600a2c7d4c3baa62b5
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-06-09 22:56:47 +0530

    Update subarray-sums-divisible-by-k.js

--- subarray-sums-divisible-by-k.js
+++ subarray-sums-divisible-by-k.js
@@ -1,1 +1,17 @@
+var subarraysDivByK = function (arr, k) {
+    let sum = 0;
+    let hash = new Map();
+    hash.set(0, 0);
 
+    let res = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] >= 0) sum += (arr[i]);
+        else sum += k + (arr[i] % k);
+
+        if (!hash.has(sum % k)) hash.set(sum % k, 0);
+        else hash.set(sum % k, hash.get(sum % k) + 1);
+        res += hash.get(sum % k);
+        sum = sum % k;
+    }
+    return res;
+};
