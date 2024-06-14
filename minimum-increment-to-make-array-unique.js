da751b11513fb07517170b42c8a1026bfec6ceab
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-06-14 22:50:56 +0530

    Update minimum-increment-to-make-array-unique.js

--- minimum-increment-to-make-array-unique.js
+++ minimum-increment-to-make-array-unique.js
@@ -1,1 +1,13 @@
+var minIncrementForUnique = function (nums) {
+    let sort = nums.sort((a, b) => a - b);
+    let moves = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] <= nums[i - 1]) {
+            let gap = nums[i - 1] - nums[i] + 1;
+            nums[i] += gap;
+            moves += gap;
+        }
+    }
 
+    return moves;
+};
