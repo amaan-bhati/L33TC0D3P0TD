63687ae058d2e35ea461c45cb63474b695d04654
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-06-16 23:38:49 +0530

    Update patching-array.js

--- patching-array.js
+++ patching-array.js
@@ -1,1 +1,10 @@
-
+var minPatches = function (nums, n) {
+    var covered = 1, count = 0, i = 0;
+    while (covered <= n) {
+        if (i >= nums.length || covered < nums[i]) {
+            count++;
+            covered += covered;
+        } else covered += nums[i++];
+    }
+    return count;
+};
