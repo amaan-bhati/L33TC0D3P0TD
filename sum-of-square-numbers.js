f6cc6edd3417c5f703b69a049f8d82c5000f2232
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-06-17 22:40:38 +0530

    Update sum-of-square-numbers.js

--- sum-of-square-numbers.js
+++ sum-of-square-numbers.js
@@ -1,1 +1,8 @@
-
+var judgeSquareSum = function(c) {
+    const sqrt = Math.floor(Math.sqrt(c))
+    for(let i=sqrt; i>=0;i--) {
+        let tmp = c
+        if(Math.sqrt(tmp-i*i) % 1 === 0) return true
+    }
+    return false
+};
