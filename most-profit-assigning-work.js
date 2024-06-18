a49a03159d125c72fb37f8106dc9e0d38653f2fd
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-06-18 23:07:04 +0530

    Update most-profit-assigning-work.js

--- most-profit-assigning-work.js
+++ most-profit-assigning-work.js
@@ -1,1 +1,16 @@
-
+var maxProfitAssignment = function (diff, profit, worker) {
+    if (Math.max(...worker) < Math.min(...diff)) {
+        return 0;
+    }
+    let money = 0;
+    for (let i = 0; i < worker.length; i++) {
+        let mmax = 0;
+        for (let j = 0; j < diff.length; j++) {
+            if (diff[j] <= worker[i] && profit[j] > mmax) {
+                mmax = profit[j];
+            }
+        }
+        money += mmax;
+    }
+    return money;
+};
