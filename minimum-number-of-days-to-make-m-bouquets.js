d20826c9cf228254ff59138ff1c353c9bcaa4441
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-06-19 12:46:32 +0530

    Update minimum-number-of-days-to-make-m-bouquets.js

--- minimum-number-of-days-to-make-m-bouquets.js
+++ minimum-number-of-days-to-make-m-bouquets.js
@@ -1,1 +1,38 @@
+var minDays = function (bloomDay, m, k) {
+    if (m * k > bloomDay.length) {
+        return -1;
+    }
 
+    let left = Math.min(...bloomDay);
+    let right = Math.max(...bloomDay);
+
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+        if (canMakeBouquets(bloomDay, mid, m, k)) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return left;
+}
+
+function canMakeBouquets(bloomDay, day, m, k) {
+    let bouquets = 0;
+    let flowers = 0;
+
+    for (let bloom of bloomDay) {
+        if (bloom <= day) {
+            flowers += 1;
+            if (flowers == k) {
+                bouquets += 1;
+                flowers = 0;
+            }
+        } else {
+            flowers = 0;
+        }
+    }
+
+    return bouquets >= m;
+};
