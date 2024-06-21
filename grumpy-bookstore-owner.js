351b90a02cc3c29d5af0ef4af60aec48dec35e40
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-06-21 22:48:01 +0530

    Update grumpy-bookstore-owner.js

--- grumpy-bookstore-owner.js
+++ grumpy-bookstore-owner.js
@@ -1,1 +1,24 @@
+var maxSatisfied = function (customers, grumpy, minutes) {
+    let ans = 0, max = 0;
+    for (let i = minutes; i < customers.length; i++) {
+        ans += customers[i] * (1 - grumpy[i]);
+    }
+    let i = 0, j = 0;
+    while (j < minutes) {
+        ans += customers[j++];
+    }
+    max = Math.max(ans, max);
+    while (j < customers.length) {
 
+        if (grumpy[i] === 1) {
+            ans -= customers[i];
+        }
+        if (grumpy[j] === 1) {
+            ans += customers[j];
+        }
+        i++;
+        j++;
+        max = Math.max(ans, max);
+    }
+    return max;
+};
